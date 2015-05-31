714702b1c0496f3ced3c296407383daa53d858d9
Sevket YILMAZ <sevketyilmaz@gmail.com>
2015-05-31 14:38:05 +0300

    Main and Child OBJECT MODELS added.

---
+++ app/src/main/java/tr/com/svkt/expert_android_chapter04/models/ChildObject.java
@@ -0,0 +1,15 @@
+package tr.com.svkt.expert_android_chapter04.models;
+
+import java.util.UUID;
+
+public class ChildObject {
+    public String name;
+    public int age;
+    public boolean likesVeggies = false;
+
+    public ChildObject(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
---
+++ app/src/main/java/tr/com/svkt/expert_android_chapter04/models/MainObject.java
@@ -0,0 +1,11 @@
+package tr.com.svkt.expert_android_chapter04.models;
+
+import java.util.ArrayList;
+
+public class MainObject {
+    public int intValue = 5;
+    public String stringValue = "st<ri>\"ng\"Value<node1>test</node2>";
+    public String[] stringArray;
+    public ArrayList<ChildObject> childList = new ArrayList<ChildObject>();
+
+}
