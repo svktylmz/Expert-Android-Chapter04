bd72d73e622fa088093dd9b2e717359bea53aa24
Sevket YILMAZ <sevketyilmaz@gmail.com>
2015-05-31 17:21:30 +0300

    Saving and restoring strings using SharedPreferences.

--- app/src/main/java/tr/com/svkt/expert_android_chapter04/GSONFunctionTester.java
+++ app/src/main/java/tr/com/svkt/expert_android_chapter04/GSONFunctionTester.java
@@ -1,5 +1,7 @@
 package tr.com.svkt.expert_android_chapter04;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import com.google.gson.Gson;
@@ -22,4 +24,37 @@ public class GSONFunctionTester {
         String compareResult = MainObject.checkTestMainObject(mo1);
         Log.i(TAG, compareResult);
     }
+
+    //Use a XML file called myprefs.xml for this example as a SharedPreferences
+    private SharedPreferences getSharedPreferences(){
+        SharedPreferences sp = MyApplication.sApplicationContext.
+                getSharedPreferences("myprefs", Context.MODE_PRIVATE);
+
+        return sp;
+    }
+
+    public void testEscapeCharactersInPreferences(){
+        String testString = "<node1>blabhh</node1>";
+        SharedPreferences sp = getSharedPreferences();
+
+        SharedPreferences.Editor spe = sp.edit(); //prepare SharedPreferences for save
+        spe.putString("test", testString); //add key value pair
+        spe.commit(); //commit the changes for persistence
+
+        //Retrieve what is stored
+        String savedString = sp.getString("test", null);
+
+        if(savedString == null){
+            Log.d(TAG, "No saved string!");
+            return;
+        }
+        Log.d(TAG, savedString);
+        if(testString.equals(savedString)){
+            Log.d(TAG, "Saved the string properly and They Match.");
+            return;
+        }
+        Log.d(TAG, "TestString and SavedSting don't match.");
+        return;
+    }
+
 }
