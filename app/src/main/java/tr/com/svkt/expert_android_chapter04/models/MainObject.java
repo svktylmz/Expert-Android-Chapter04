c18d407ad2b95ad4d4fa588e10a70f51a68bdb78
Sevket YILMAZ <sevketyilmaz@gmail.com>
2015-05-31 14:46:27 +0300

    Added functionality to MainObject

--- app/src/main/java/tr/com/svkt/expert_android_chapter04/models/MainObject.java
+++ app/src/main/java/tr/com/svkt/expert_android_chapter04/models/MainObject.java
@@ -8,4 +8,43 @@ public class MainObject {
     public String[] stringArray;
     public ArrayList<ChildObject> childList = new ArrayList<ChildObject>();
 
+    public void addChild(ChildObject childObject){
+        childList.add(childObject);
+    }
+
+    public void populateStringArray(){
+        stringArray = new String[2];
+        stringArray[0] = "first";
+        stringArray[1] = "second";
+    }
+
+    //This method use to create sample MainObject
+    public static MainObject createTestMainObject(){
+        MainObject mo = new MainObject();
+        mo.populateStringArray();
+        mo.addChild(new ChildObject("Eve", 30));
+        mo.addChild(new ChildObject("Adam" , 28));
+        return mo;
+    }
+
+    //this method is used to verify two MainObject instances are the same.
+    public static String checkTestMainObject(MainObject mo) {
+        MainObject moCopy = createTestMainObject();
+        if (!(mo.stringValue.equals(moCopy.stringValue)))
+        {
+            return "String values don't match:" + mo.stringValue;
+        }
+        if (mo.childList.size() != moCopy.childList.size())
+        {
+            return "array list size doesn't match";
+        }
+        //get first child
+        ChildObject firstChild = mo.childList.get(0);
+        ChildObject firstChildCopy = moCopy.childList.get(0);
+        if (!firstChild.name.equals(firstChildCopy.name))
+        {
+            return "first child name doesnt match";
+        }
+        return "everything matches";
+    }
 }
