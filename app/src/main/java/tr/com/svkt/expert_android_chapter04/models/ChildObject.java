98932a608007d9d0d85adeeb64f577e5f0190fe5
Sevket YILMAZ <sevketyilmaz@gmail.com>
2015-05-31 14:38:44 +0300

    Private field added to ChildObject model

--- app/src/main/java/tr/com/svkt/expert_android_chapter04/models/ChildObject.java
+++ app/src/main/java/tr/com/svkt/expert_android_chapter04/models/ChildObject.java
@@ -3,6 +3,7 @@ package tr.com.svkt.expert_android_chapter04.models;
 import java.util.UUID;
 
 public class ChildObject {
+    private UUID id = UUID.randomUUID();
     public String name;
     public int age;
     public boolean likesVeggies = false;
@@ -12,4 +13,11 @@ public class ChildObject {
         this.age = age;
     }
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
 }
